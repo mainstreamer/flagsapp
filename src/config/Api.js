9f5dbd876d7f463bbc132bcc4ff8722190ffe79d
Artem Dolmatov <artem@f6s.com>
2021-01-22 23:28:11 +0200

    docke config for beckend env

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,6 +1,8 @@
 const api = {
     // 'url' : 'https://flags.izeebot.top',
+    // 'url' : 'http://0.0.0.0',
     'url' : 'http://localhost',
+    // 'url' : 'http://127.0.0.1',
     // 'url' : 'http://localhost:8000',
     // 'mode' : 'prod',
     'mode' : 'dev'
